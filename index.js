b90dd76c6793556200063d3f119a1737da403832
lcfangue <lcfangue@gmail.com>
2022-03-03 22:49:57 +0000

    lowdown

--- index.js
+++ index.js
@@ -28,10 +28,83 @@ module.exports.each = each;
  * passed to the function.
  * 
  * @param {Number, String, Boolean, Object, etc} value: The value that will be 
- * returned.
+ * returned, unaltered.
  */
  function identity(value) {
     return value;
 }
 module.exports.identity = identity;
 
+/**
+ * typeOf: Designed to take in a paramater and return its data type as a string.
+ * 
+ * @param {Number, String, Boolean, Object, etc} value: the given value whose 
+ * data type will be returned.
+ */
+function typeOf(value) {
+    if (Array.isArray(value)) {
+        return "array";
+    }
+    if (value === null) {
+        return "null";
+    }
+    return typeof value;
+}
+module.exports.typeOf = typeOf;
+
+/**
+ * first: Designed to take in an array and a number, and return the first 
+ * <number> of elements in that array.
+ * 
+ * @param {Array} array: The array whose first elements will be returned
+ * @param {Number} number: The number of elements that will be returned from
+ * the given array.
+ */
+function first(array, number) {
+    if (!Array.isArray(array)) {
+        return [];
+    }
+    if (!number || number === NaN) {
+        return array[0];
+    } 
+    if (number < 0) {
+        return [];
+    }
+    if (number > array.length) {
+        return array;
+    }
+    var newArr = array.slice(0, array.length - 1);
+    return newArr;
+    
+}
+module.exports.first = first;
+
+/**
+ * last: Designed to take in an array and a number, and return the last 
+ * <number> of elements in that array.
+ * 
+ * @param {Array} array: The array whose last elements will be returned
+ * @param {Number} number: The number of elements that will be returned
+ * from the given array.
+ */
+function last(array, number) {
+    if (!Array.isArray(array)) {
+        return [];
+    }
+    if (!number || number === NaN) {
+        return array[array.length - 1];
+    }
+    if (number < 0) {
+        return [];
+    }
+    if (number > array.length) {
+        return array;
+    }
+    var newArr = array.slice(array.length);
+    return array.slice(1, array.length);
+}
+module.exports.last = last;
+
+/**
+ * indexOf: 
+ */
